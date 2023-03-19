410bc8611137f031b8aace8702d0455098381e70
sasamori.yu <sasa0330y@gmail.com>
2023-03-19 16:35:11 +0900

    詳細画面ざっくり/商品情報はsessionStrageに変更

--- src/components/organisms/MainDetail.tsx
+++ src/components/organisms/MainDetail.tsx
@@ -1,29 +1,51 @@
 /**
  * 詳細画面
  */
-import { useContext } from "react";
 import { useParams } from "react-router-dom";
-import { ShopListContext } from "../../providers/ShopListProvider";
+import { TypeShopList } from "../../type/globalTypes";
+import { css } from "@emotion/css";
 
+const style = {
+  img: css`
+    text-align: center;
+    margin-bottom: 20px;
+  `,
+  shopName: css`
+    font-size: 30px;
+    font-weight: bold;
+  `,
+};
 export const MainDetail = () => {
-  const { shopList } = useContext(ShopListContext);
+  //URLパラメータのitemId取得
   const { itemId } = useParams();
+  //セッションストレージの一覧情報オブジェクト取得
+  const shopListString = sessionStorage.getItem("shopListStorage");
+  const shopList: TypeShopList[] = shopListString
+    ? JSON.parse(shopListString)
+    : null;
+  let shopObj: Pick<
+    TypeShopList,
+    "shopName" | "photoPcM" | "budgetName" | "lunch" | "address"
+  > = {
+    shopName: "",
+    photoPcM: "",
+    budgetName: "",
+    lunch: "",
+    address: "",
+  };
+  shopList.forEach((shop: TypeShopList) => {
+    if (shop.itemId === itemId) return (shopObj = shop);
+  });
 
   return (
     <>
-      {shopList.forEach((shop) => {
-        if (shop.itemId === itemId) {
-          console.log("取得できた！");
-          return (
-            <>
-              <div>{shop.shopName}</div>
-              <div>{shop.budgetName}</div>
-              <div>{shop.lunch}</div>
-              <div>{shop.address}</div>
-            </>
-          );
-        }
-      })}
+      <div className={style.img}>
+        <img src={shopObj.photoPcM} alt={shopObj.shopName} />
+      </div>
+      <div className={style.shopName}>{shopObj.shopName}</div>
+      <div>{shopObj.budgetName}</div>
+      <div>{shopObj.lunch}</div>
+      <div>{shopObj.address}</div>
     </>
   );
 };
